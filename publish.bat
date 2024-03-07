58a6992d8a95165bd70aa7fd5f65e998998aee3f
Olivier Rey <rey.olivier@gmail.com>
2024-03-07 16:27:33 +0100

    SRD with bookmarks

---
+++ publish.bat
@@ -0,0 +1,16 @@
+cls
+@echo off
+
+if "%1" == "" ( 
+    echo Git publish script version 1
+    echo Usage : publish.bat [comments without quotes]
+    goto :end
+)
+
+git add *
+git commit -a -m "%*"
+git push origin main
+
+:end
+echo Done
+
